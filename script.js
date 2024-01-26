ee8976f3b373273a46a5898c8adaacbc6681dc5c
Amrita Patil <157470200+Amrita1885@users.noreply.github.com>
2024-01-26 13:01:38 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,21 @@
+let display = document.getElementById('display');
+
+function appendToDisplay(value) {
+  if (display.innerHTML === '0') {
+    display.innerHTML = '';
+  }
+  display.innerHTML += value;
+}
+
+function clearDisplay() {
+  display.innerHTML = '0';
+}
+
+function calculateResult() {
+  try {
+    display.innerHTML = eval(display.innerHTML);
+  } catch (error) {
+    display.innerHTML = 'Error';
+  }
+}
+ 
